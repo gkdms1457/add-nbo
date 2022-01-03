51d4946705030e0560f9d0c79d56388285becdb7
gkdms1457 <gkdms1457@icloud.com>
2022-01-03 21:49:58 +0900

    code refactoring

--- add.c
+++ add.c
@@ -1,19 +1,42 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+
+uint32_t readint (char* filename) {
+
+	File *file = fopen(filename, "rb");
+	uint32_t f;
+	size_t readlen = fread(&f, 1, sizeof(uint32_t), file);
+
+	if (readlen != sizeof(uint32_t)) {
+		
+		fprintf(stderr, "fread return %lu\n", readlen);
+		exit(-1);
+
+	}
+
+	f = ntohl(f);
+	fclose(file);
+
+	return f;
+
+}
 
 int main(int argc, char **argv){
 	
-	FILE *fp1 = fopen(argv[1],"rb");
-	FILE *fp2 = fopen(argv[2],"rb");
-	uint32_t a1;
-	uint32_t a2;
-//	uint32_t result;
+	if (argc != 3) {
 
-	fread(&a1,sizeof(a1),1,fp1);
-	fread(&a2,sizeof(a2),1,fp2);
-	a1=htonl(a1);
-	a2=htonl(a2);
-	printf("1000(0x%0x) + 500(0x%0x) = 1500(0x%0x)",a1,a2,a1+a2);
+		fprintf(stderr, "syntax : add-nbo <file1> <file2>\n");
+		exit(-1);
+	}
+
+	uint32_t f1, f2;
+
+	f1 = readint(argv[1]);
+	f2 = readint(argv[2]);
+
+	uint32_t result = f1 + f2;
+	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", f1, f1, f2, f2, result,result);
 
 }
